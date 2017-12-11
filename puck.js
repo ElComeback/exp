e65e9d0859071060cf61ba5c1ab9219b228363ba
ElComeback <34289220+ElComeback@users.noreply.github.com>
2017-12-11 15:32:06 -0600

    Update puck.js

--- puck.js
+++ puck.js
@@ -13,7 +13,9 @@ client.on('warn', console.warn);
 
 client.on('error', console.error);
 
-client.on('ready', () => console.log('Listo!'));
+client.on('ready', () => console.log('Listo!');
+client.user.setGame(prefix+'ayuda |´∀｀●) ');
+);
 
 client.on('disconnect', () => console.log('Desconectado del Server, Reconectando...'));
 
