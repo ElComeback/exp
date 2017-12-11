11dad7d3b3dd6e68139c0d5ba9e99342c89e3806
ElComeback <34289220+ElComeback@users.noreply.github.com>
2017-12-10 20:10:47 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.TOKEN = 'MzgwOTM4NjkzMTQ3MzYxMjkw.DQ8tiQ.hwMtI08g8cVnCYT_vuQeYxVt1z8';
+exports.TOKEN = 'MzgwOTM4NjkzMTQ3MzYxMjkw.DQ950Q.pEp_cQhU_Ay-Bbj-VYoH_J_G6rQ';
 
 exports.PREFIX = 'm!';
 
