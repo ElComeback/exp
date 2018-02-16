f37862db41cc3a2ed29bcd408e8eaee1bc67881f
ElComeback <34289220+ElComeback@users.noreply.github.com>
2018-02-16 12:22:42 -0600

    Update puck.js

--- puck.js
+++ puck.js
@@ -13,8 +13,13 @@ client.on('warn', console.warn);
 client.on('error', console.error);
 
 client.on('ready', () => {
-  client.user.setGame('~+ayuda |´∀｀●)')
+  client.user.setPresence({ game: { name: '~+ayuda |´∀｀●)', type: 1 } });
 })
+                                                                                               /*type=0 = playing  
+                                                                                                 type=1 = streaming  
+                                                                                                  type=2 = listening 
+                                                                                                   type=3 = watching﻿*/
+
 
 client.on('disconnect', () => console.log('Desconectado del Server, Reconectando...'));
 
