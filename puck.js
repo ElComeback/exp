7d446cd7e4ecb9ddc289e305dfc50983371a1822
ElComeback <34289220+ElComeback@users.noreply.github.com>
2018-02-16 12:50:09 -0600

    Update puck.js

--- puck.js
+++ puck.js
@@ -13,7 +13,7 @@ client.on('warn', console.warn);
 client.on('error', console.error);
 
 client.on('ready', () => {
-  client.user.setPresence({ game: { name: '~+ayuda |´∀｀●)', type: 2 } })
+  client.user.setPresence({ game: { name: '~+ayuda |´∀｀●)', type: 1 } })
 });
                                                                                                /*type=0 = playing  
                                                                                                  type=1 = streaming  
