b0a158904d0f3eaa25d354506486729ff676c157
ElComeback <34289220+ElComeback@users.noreply.github.com>
2019-07-21 17:38:16 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = 'NjAyMjExODE2MTc2NzQ2NTg3.XTN2bw.rBFfuEXwCM88jLZNUkNnEdtAebE';
 
 exports.PREFIX = '~';
 
-exports.GOOGLE_API_KEY = 'AIzaSyDfqE5oyOcMXrj1ycNnpDqpn2rlK98rUO8';
+exports.GOOGLE_API_KEY = 'AIzaSyCdyZnP9gu0KUJfOHtxVc9S8zFKpYzp_c8';
