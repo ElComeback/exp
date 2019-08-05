b8ff4a325b003be6a5e4988f3cc24763f14fe428
ElComeback <34289220+ElComeback@users.noreply.github.com>
2019-08-04 23:41:56 -0500

    Update puck.js

--- puck.js
+++ puck.js
@@ -7,7 +7,7 @@ const youtube = new YouTube(GOOGLE_API_KEY);
 const queue = new Map();
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const guild = require ('discord.js')
+const guild = require ('discord.js');
 
 
 client.on('warn', console.warn);
