c6ce4ef1599c557b73ea7573670876f0e4d8fe58
ElComeback <34289220+ElComeback@users.noreply.github.com>
2019-08-05 00:46:27 -0500

    Update puck.js

--- puck.js
+++ puck.js
@@ -370,8 +370,10 @@ const guild = require ("discord.js");
 
 client.on("message", (message) => { if (message.content.startsWith(PREFIX + "test")) { 
     message.guild.createChannel("Escribiendo...", "text")
-	message.channel.edit({ name: 'Hora de Mexico: '`${mod_hora}` })
- }});
+	setTimeout(function(){
+    message.guild.channels.find("name", "Escribiendo...").setName("'Hora de Mexico: '`${mod_hora}`") 
+}, 2000)
+}});
 
 //Modulo de Musica
 
